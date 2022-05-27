9b4d33970da443e35db7ff5fd02e29a3e1935c97
Tiffany Frierson <QuantumP7@gmail.com>
2022-05-27 15:02:10 -0400

    Add files via upload

---
+++ campaign.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.8.14;
+
+contract Campaign {
+    struct Request{
+        string description;
+        uint value;
+        address payable recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+    
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    mapping(uint => Request) requests;
+    uint numRequests;
+    uint public approverCount;
+
+    function Campaign (uint minimum) public{
+        //The manager variable will be set to whoever deploys the contract
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function contribute() public payable{
+        require(msg.value > minimumContribution);
+        approvers[msg.sender] = true;
+        approverCount++;
+    }
+
+    function createRequest(string descriptionIn, uint valueIn, address payable recipientIn) public restricted{
+        //Get the last index of requests from storage
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        
+        newRequest.description = descriptionIn;
+        newRequest.value = valueIn;
+        newRequest.recipient = recipientIn;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+    }
+
+    function approveRequest(uint index) public {
+        //Sender must be on the list of approvers
+        require(approvers[msg.sender]);
+        //Sender must not have voted yet
+        require(!(requests[index].approvers[msg.sender]));
+
+        Request storage request = requests[index];
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public payable restricted{
+        //Make sure that the request hasn't already been finalized
+        require(!(requests[index].complete));
+
+        Request storage request = requests[index];
+        //Make sure that the number of yes votes is greater than half of the number of approvers
+        require(request.approvalCount > (approverCount / 2));
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
