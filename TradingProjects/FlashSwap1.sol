3ed331ea43af7b0396db145323c415c5b12ec1e5
Tiffany Frierson <QuantumP7@gmail.com>
2022-06-23 06:17:01 -0400

    Update FlashSwap1.sol

--- TradingProjects/FlashSwap1.sol
+++ TradingProjects/FlashSwap1.sol
@@ -53,12 +53,9 @@ contract PancakeFlashSwap{
         //Get the amounts required for the swap to happen!
         uint256 amountRequired = IUniswapV2Router01(PANCAKEROUTER).getAmountsOut(_amountIn, path)[1];
 
-        console.log("Amount required: ", amountRequired);
-
         //Performs Arbitrage -Swap for another token
         uint256 amountReceived = IUniswapV2Router01(PANCAKEROUTER).swapExactTokensForTokens(_amountIn, 
                 amountRequired, path, address(this), deadline)[1];
-        console.log("Amount received after swap: ", amountReceived);
 
         require(amountReceived > 0, "Aborted transaction: Trade returned zero");
         return amountReceived;
@@ -90,14 +87,14 @@ contract PancakeFlashSwap{
         uint256 amount1Out = _tokenToBorrow == token1 ? _amountToBorrow : 0;
 
         //Pass the data as bytes so that the swap function knows this is a flash swap (refer to documentation)
-        bytes memory data = abi.encode(_tokenToBorrow, _amountToBorrow);
+        bytes memory data = abi.encode(_tokenToBorrow, _amountToBorrow, msg.sender);
 
         //Execute the initial swap to get the loan
         IUniswapV2Pair(pair).swap(amount0Out, amount1Out, address(this), data);  //This is going to call the upcoming pancakeCall function
     }
 
     function pancakeCall(address _sender, uint256 _amount0, uint256 _amount1, bytes calldata _data) external{
-        //Ensure this request came from the contract
+        //Ensure this request came from the contract (from the swap call)
         address token0 = IUniswapV2Pair(msg.sender).token0();
         address token1 = IUniswapV2Pair(msg.sender).token1();
         address pair = IUniswapV2Factory(PANCAKEFACTORY).getPair(token0, token1);
@@ -106,7 +103,7 @@ contract PancakeFlashSwap{
         require(_sender == address(this), "The sender should match this contract");
 
         //Decode the data for calculating the repayment
-        (address tokenBorrow, uint256 amount) = abi.decode(_data, (address, uint256));
+        (address tokenBorrow, uint256 amount, address myAddress) = abi.decode(_data, (address, uint256, address));
 
         //Calculate the amount to repay at the end
         uint256 fee = ((amount * 3) / 197) + 1;
@@ -124,8 +121,8 @@ contract PancakeFlashSwap{
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
         //PAY YOURSELF FIRST!!!
-        require(profitableTrade(arbitrageResult, amountToPay), "Trade was not profitable!");
-        IERC20(tokenBorrow).transfer(pair, (arbitrageResult - amountToPay));
+        require(profitableTrade(arbitrageResult, amountToPay + tx.gasprice), "Trade was not profitable!");
+        IERC20(tokenBorrow).transfer(myAddress, (arbitrageResult - (amountToPay + tx.gasprice)));
 
         //NOW PAY THE LOAN BACK!!!
         IERC20(tokenBorrow).transfer(pair, amountToPay);
